24cdd19bc09de5619cd4b80a755f54b23aec4b2b
Tanatozzz Tanatozzz <ubernikita999@gmail.com>
2022-09-30 15:25:31 +0300

    1

--- LocalChat/Windows/LoginWindow.xaml.cs
+++ LocalChat/Windows/LoginWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using LocalChat.Windows;
+﻿using LocalChat.Classes;
+using LocalChat.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,17 +22,64 @@ namespace LocalChat
     /// </summary>
     public partial class LoginWindow : Window
     {
+        private bool CurrentOnlineStatus;
         public LoginWindow()
         {
             InitializeComponent();
         }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void ButtonMinimaze_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMaximazed_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState != WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Maximized;
+                this.ResizeMode = ResizeMode.NoResize;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            //CurrentOnlineStatus = false;
+            Class1.Context.SaveChanges();
+            Application.Current.Shutdown();
+            
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow chatWindow = new MainWindow();
-            this.Close();
-            chatWindow.Show();
+            var authUser = Classes.Class1.Context.User.ToList()
+                .Where(i => i.Nickname.Equals(txtLog.Text)).FirstOrDefault();
+            //CurrentOnlineStatus = authUser.OnlineStatus;
+            if (authUser != null)
+            {
+                CurrentOnlineStatus = true;
+                Class1.Context.SaveChanges();
+                MainWindow chatWindow = new MainWindow(authUser);
+                this.Close();
+                
+                chatWindow.Show();
 
+            }
+            else
+            {
+                MessageBox.Show("Пользовател не найден");
+            }
         }
     }
 }
--- LocalChat/Windows/MainWindow.xaml.cs
+++ LocalChat/Windows/MainWindow.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace LocalChat.Windows
 {
@@ -19,11 +13,12 @@ namespace LocalChat.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        public MainWindow()
+        private int currentUserID;
+        public MainWindow(EF.User authUser)
         {
             
             InitializeComponent();
+            currentUserID = authUser.ID;
             List<EF.User> users = new List<EF.User>();
             users = Classes.Class1.Context.User.ToList();
             lvUsers.ItemsSource = users;
@@ -67,18 +62,7 @@ namespace LocalChat.Windows
 
         private void lvUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var smth = (lvUsers.SelectedItem as EF.User);
 
-            var userD = smth.ID;
-            List<EF.User> UserSelected = new List<EF.User>();
-            List<EF.ChatUser> ChatUserSelected = new List<EF.ChatUser>();
-            List<EF.Chat> ChatSelected = new List<EF.Chat>();
-            List<EF.Message> MessageSselected = new List<EF.Message>();
-            var chatuser1 = ChatUserSelected.Where(i =>
-            i.IDUser == userD).ToList();
-            lvChat.ItemsSource = chatuser1;
-            //var chat13 = MessageSselected.Where(i => i.IDChat == (chat1)).ToList();
-            //var allMessages = MessageSselected.Where(i => i.IDChat == (chat1)).ToList();
         }
     }
 }
